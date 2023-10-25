8ef470be0800cd3ee7ebb5320a0ad64b51b64ff6
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2023-10-24 21:50:36 -0400

    Ideas

--- src/zpl.c
+++
@@ -1,9 +0,0 @@
-//Tidal2D by Sebastien MacDougall-Landry
-//License is available at
-//https://github.com/EmperorPenguin18/tidal2d/blob/main/LICENSE
-
-#define ZPL_IMPL
-#define ZPL_PICO
-#define ZPL_ENABLE_CORE
-#define ZPL_ENABLE_PARSER
-#include "zpl.h"
